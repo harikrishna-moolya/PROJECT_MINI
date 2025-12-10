d5f3029534c06c2a9fd80a6a0bd6a69a4d294bec
harikrishna-moolya <hari.krishna@moolya.com>
2025-12-10 23:48:36 +0530

    TestUtils.java

--- src/main/java/com/project/utils/TestUtils.java
+++ src/main/java/com/project/utils/TestUtils.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.WebDriver;
 
 public class TestUtils {
     public static void safeQuit(WebDriver driver){
-        try { if(driver!=null) driver.quit(); } catch(Exception ignored){}
+
+        try { if(driver!=null) driver.quit(); }
+        catch(Exception ignored){}
     }
 }
