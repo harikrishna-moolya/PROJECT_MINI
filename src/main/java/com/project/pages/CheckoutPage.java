538e6fcf04ed32dd8d17eee8fcce9c15d22be2f9
harikrishna-moolya <hari.krishna@moolya.com>
2025-12-10 22:28:53 +0530

    CheckoutPage.java

--- src/main/java/com/project/pages/CheckoutPage.java
+++ src/main/java/com/project/pages/CheckoutPage.java
@@ -1,8 +1,21 @@
 package com.project.pages;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class CheckoutPage {
-    WebDriver driver;
-    public CheckoutPage(WebDriver driver){ this.driver = driver; }
-    // Implement checkout steps as needed for demo flows
+
+    private WebDriver driver;
+
+    // Locators (placeholder)
+    private By checkoutButton = By.id("checkout");
+
+    public CheckoutPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Click checkout (for demo placeholder)
+    public void clickCheckout() {
+        driver.findElement(checkoutButton).click();
+    }
 }
