a5787ce1070801ebec5ac44e506c786920a99505
harikrishna-moolya <hari.krishna@moolya.com>
2026-01-20 12:05:19 +0530

    Update BaseTest.java

--- src/test/java/com/project/tests/BaseTest.java
+++ src/test/java/com/project/tests/BaseTest.java
@@ -1,20 +1,28 @@
 package com.project.tests;
+
 import com.project.driver.BrowserFactory;
 import com.project.utils.ConfigReader;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.*;
 
 public class BaseTest {
+
     protected WebDriver driver;
+
+    @Parameters("browser")
     @BeforeMethod
-    public void setUp(){
-        driver = BrowserFactory.getDriverIncognito();
-        String url = ConfigReader.get("url"); 
-        if(url!=null) driver.get(url); else driver.get("https://demowebshop.tricentis.com/");
+    public void setUp(@Optional("chrome") String browser) {
+
+        driver = BrowserFactory.getDriver(browser);
+
+        String url = ConfigReader.get("url");
+        driver.get(url != null ? url : "https://demowebshop.tricentis.com/");
     }
+
     @AfterMethod
-    public void tearDown(){
-        if(driver!=null) driver.quit();
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
