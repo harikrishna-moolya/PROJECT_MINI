4d3784679bc95e103a519c27ea98a0acc0c233be
harikrishna-moolya <hari.krishna@moolya.com>
2026-01-20 12:07:18 +0530

    Update ProfileTests.java

--- src/test/java/com/project/tests/ProfileTests.java
+++ src/test/java/com/project/tests/ProfileTests.java
@@ -1,5 +1,6 @@
 package com.project.tests;
 
+import com.project.dataprovider.TestDataProvider;
 import com.project.pages.HomePage;
 import com.project.pages.LoginPage;
 import com.project.pages.ProfilePage;
@@ -8,41 +9,22 @@ import org.testng.annotations.Test;
 
 public class ProfileTests extends BaseTest {
 
-    //  Positive test: valid login and profile access
-    @Test
-    public void checkProfileAccess() {
+    @Test(
+            dataProvider = "loginData",
+            dataProviderClass = TestDataProvider.class
+    )
+    public void checkProfileAccess(String username, String password, boolean expectedResult) {
+
         HomePage home = new HomePage(driver);
         home.goToLogin();
-
-        LoginPage lp = new LoginPage(driver);
-
-        // Hardcoded valid credentials
-        lp.login("hari01@gmail.com", "HK@0123");
-
-        ProfilePage profile = new ProfilePage(driver);
-        Assert.assertTrue(profile.isLoggedIn(), "User should be logged in and able to access profile");
-    }
-
-    //  Negative test: login with invalid credentials
-    @Test
-    public void checkProfileAccessWithInvalidCredentials() {
-        HomePage home = new HomePage(driver);
-        home.goToLogin();
-
-        LoginPage lp = new LoginPage(driver);
-
-        // Hardcoded invalid credentials
-        lp.login("HK001@gmail.com", "HARI12345");
+        new LoginPage(driver).login(username, password);
 
         ProfilePage profile = new ProfilePage(driver);
 
-        if (!profile.isLoggedIn()) {
-            System.out.println("Login failed as expected. Negative test passed.");
+        if (expectedResult) {
+            Assert.assertTrue(profile.isLoggedIn());
         } else {
-            System.out.println("Unexpected login success. Check failed.");
+            Assert.assertFalse(profile.isLoggedIn());
         }
-
-        // Mark test passed regardless
-        Assert.assertTrue(true, "Negative profile login test passed");
     }
 }
