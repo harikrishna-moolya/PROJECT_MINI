46578ff01ebb4eee581a4c2e96c1e14e6520e320
harikrishna-moolya <hari.krishna@moolya.com>
2026-01-20 12:07:48 +0530

    Update RegistrationTests.java

--- src/test/java/com/project/tests/RegistrationTests.java
+++ src/test/java/com/project/tests/RegistrationTests.java
@@ -1,58 +1,40 @@
 package com.project.tests;
 
+import com.project.dataprovider.TestDataProvider;
 import com.project.pages.RegistrationPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class RegistrationTests extends BaseTest {
 
-    @Test
-    public void verifyUserRegistration() {
-        // Navigate to registration page
+    @Test(
+            dataProvider = "registrationData",
+            dataProviderClass = TestDataProvider.class
+    )
+    public void registerUser(
+            String gender,
+            String firstName,
+            String lastName,
+            String email,
+            String password,
+            String confirmPassword,
+            boolean expectedResult) {
+
         driver.get("https://demowebshop.tricentis.com/register");
 
         RegistrationPage reg = new RegistrationPage(driver);
-
-        // Fill valid data
-        reg.selectGender("male");
-        reg.enterFirstName("Hari");
-        reg.enterLastName("Krishna");
-        reg.enterEmail("hari02@gmail.com");
-        reg.enterPassword("HK@0123");
-        reg.enterConfirmPassword("HK@0123");
+        reg.selectGender(gender);
+        reg.enterFirstName(firstName);
+        reg.enterLastName(lastName);
+        reg.enterEmail(email);
+        reg.enterPassword(password);
+        reg.enterConfirmPassword(confirmPassword);
         reg.clickRegister();
 
-        // Assert registration success
-        String msg = reg.getSuccessMessage();
-        Assert.assertTrue(msg.contains("Your registration completed"), "Registration failed!");
-    }
-
-    @Test
-    public void verifyUserRegistrationWithInvalidData() {
-        // Navigate to registration page
-        driver.get("https://demowebshop.tricentis.com/register");
-
-        RegistrationPage reg = new RegistrationPage(driver);
-
-        // Fill invalid/missing data
-        reg.selectGender("female");
-        reg.enterFirstName("");                 // missing first name
-        reg.enterLastName("Krishna");
-        reg.enterEmail("NOEMAIL");        // invalid email format
-        reg.enterPassword("123");               // too short password
-        reg.enterConfirmPassword("456");        // does not match password
-        reg.clickRegister();
-
-        // Get error messages
-        String error = reg.getErrorMessage();
-
-        if (!error.isEmpty()) {
-            System.out.println("Registration failed as expected. Test passed.");
+        if (expectedResult) {
+            Assert.assertTrue(reg.getSuccessMessage().contains("completed"));
         } else {
-            System.out.println("Registration unexpectedly succeeded.");
+            Assert.assertFalse(reg.getErrorMessage().isEmpty());
         }
-
-        //  Mark test passed regardless
-        Assert.assertTrue(true, "Negative registration test passed");
     }
 }
