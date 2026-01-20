a740dc63a53fac9fa2a47d621a187103e99c809c
harikrishna-moolya <hari.krishna@moolya.com>
2026-01-20 12:06:10 +0530

    Update CheckoutTests.java

--- src/test/java/com/project/tests/CheckoutTests.java
+++ src/test/java/com/project/tests/CheckoutTests.java
@@ -1,62 +1,36 @@
 package com.project.tests;
 
+import com.project.dataprovider.TestDataProvider;
 import com.project.pages.CartPage;
 import com.project.pages.CheckoutPage;
 import com.project.pages.HomePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
 public class CheckoutTests extends BaseTest {
 
-    @Test
-    public void testCheckoutFlow() {
+    @Test(
+            dataProvider = "cartData",
+            dataProviderClass = TestDataProvider.class
+    )
+    public void checkoutFlow(String productName) {
+
+        driver.findElement(By.id("small-searchterms"))
+                .sendKeys(productName, Keys.ENTER);
+
+        driver.findElements(By.cssSelector("input[value='Add to cart']")).get(0).click();
 
         HomePage home = new HomePage(driver);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        home.openCart();
 
-        try {
-            // Search for a product
-            By searchBox = By.id("small-searchterms");
-            WebElement input = driver.findElement(searchBox);
-            input.clear();
-            input.sendKeys("14.1-inch Laptop");
-            input.sendKeys(Keys.ESCAPE); // Close dropdown
-            input.sendKeys(Keys.ENTER);  // Submit search
+        CartPage cart = new CartPage(driver);
+        Assert.assertFalse(cart.isCartEmpty());
 
-            // Wait for "Add to cart" button to be clickable
-            By addToCartBtn = By.cssSelector("input[value='Add to cart']");
-            wait.until(ExpectedConditions.elementToBeClickable(addToCartBtn));
+        cart.acceptTerms();
+        new CheckoutPage(driver).clickCheckout();
 
-            // Click the first "Add to cart" button
-            driver.findElements(addToCartBtn).get(0).click();
-
-            //  Wait for success notification to disappear
-            wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("bar-notification")));
-
-            //  Open cart
-            home.openCart();
-
-            // Accept terms of service
-            CartPage cart = new CartPage(driver);
-            cart.acceptTerms();
-
-            //  Navigate to checkout page (Demo site placeholder)
-            CheckoutPage checkout = new CheckoutPage(driver);
-            checkout.clickCheckout(); // Safe placeholder; demo site will stop before real order
-
-            // Assert we are on the checkout page
-            Assert.assertTrue(driver.getCurrentUrl().contains("checkout"), "Should navigate to checkout page");
-
-        } catch (Exception e) {
-            Assert.fail("Checkout test failed: " + e.getMessage());
-        }
+        Assert.assertTrue(driver.getCurrentUrl().contains("checkout"));
     }
 }
